85fe7c61a405701e637682f59d447a9a700b7062
sanjarjo <osanjar642@gmail.com>
2026-01-05 21:39:55 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,40 @@
+BOT_TOKEN = "8410955320:AAEOMfM7p4bkL0kt6lnwPcraoYyNivXAfRU"
+ADMIN_ID = 7390469118
+
+SMM_API_URL = "https://uzbek-seen.uz/api/v2"
+SMM_API_KEY = "ed41d84b8121526616a0113c3a97bfad"
+
+CHECK_INTERVAL = 60  # 1 daqiqa
+
+ORDERS = [
+    {
+        "name": "tiktok tekin ko'rishlar (prasmotr)",
+        "service_id": 1383,
+        "quantity": 555,
+        "link": "https://vt.tiktok.com/ZS55U431h/"
+    },
+    {
+        "name": "tiktok tekin like ",
+        "service_id": 1384,
+        "quantity": 20,
+        "link": "https://vt.tiktok.com/ZS55U431h/"
+    },
+    {
+        "name": "telegram tekin obunachi",
+        "service_id": 1381,
+        "quantity": 15,
+        "link": "https://t.me/mlbbdonatee"
+    },
+    {
+        "name": "tiktok tekin ko'rishlar (prasmotr) ",
+        "service_id": 1383,
+        "quantity": 555,
+        "link": "https://vt.tiktok.com/ZS55yA9x3/"
+    },
+    {
+        "name": "tiktok tekin like",
+        "service_id": 1384,
+        "quantity": 20,
+        "link": "https://vt.tiktok.com/ZS55yA9x3/"
+    }
+]
