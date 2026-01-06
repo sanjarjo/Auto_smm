91563c6f010c0d76691338d09da8f7fe0c08f406
sanjarjo <osanjar642@gmail.com>
2026-01-06 22:04:37 +0500

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,21 +1,14 @@
-# notifier.py
 import asyncio
 
 tg_app = None
-loop = None
 ADMIN_ID = None
 
-def init_notifier(application, event_loop, admin_id):
-    global tg_app, loop, ADMIN_ID
+def init_notifier(application, admin_id):
+    global tg_app, ADMIN_ID
     tg_app = application
-    loop = event_loop
     ADMIN_ID = admin_id
 
 def send_admin(text):
-    if not tg_app or not loop or not ADMIN_ID:
+    if not tg_app or not ADMIN_ID:
         return
-
-    asyncio.run_coroutine_threadsafe(
-        tg_app.bot.send_message(chat_id=ADMIN_ID, text=text),
-        loop
-    )
+    asyncio.create_task(tg_app.bot.send_message(chat_id=ADMIN_ID, text=text))
