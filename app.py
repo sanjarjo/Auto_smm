b1ff4b80063f054f188f9c2841d4715220f0d9bd
sanjarjo <osanjar642@gmail.com>
2026-01-06 11:32:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from telegram.ext import Application, CommandHandler
 from config import BOT_TOKEN
 
-def start(update, context):
-    update.message.reply_text("Bot ishlayapti ✅")
+async def start(update, context):
+    await update.message.reply_text("Bot ishlayapti ✅")
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
 
 if __name__ == "__main__":
